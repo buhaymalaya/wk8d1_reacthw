c81bfa7f05529c1bca228fc79a9559b23ca1f5fe
Buhay Malaya <buhaymalaya@Buhays-Air.lan>
2024-04-18 18:20:35 -0400

    states

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import { useState } from 'react';
 import '../styles/forms.css';
 
 export default function Forms() {
-    return (
-        <form>
-          <label>
-            Full Name:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Email:
-            <input type="email" name="email" />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-      );
+    const [formData, setFormData] = useState({
+        name: '',
+        email: ''
+    });
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+    }
+
+    return (
+        <div>
+            {!submitted ? (
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Full Name:
+                        <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                    </label>
+                    <label>
+                        Email:
+                        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+            ) : (
+                <div className='welcome'>
+                    <p>Welcome, {formData.name}! Please check your inbox/spam folder for your unique raffle ticket. Thank you for participating!</p>
+                </div>
+            )}
+        </div>
+    );
+}
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,8 +9,7 @@ export default function Landing() {
         <h1>Mabuhay Aloha Kapatid</h1>
       </header>
       <section className="main-content">
-        <p>The island life is a birth right.</p>
-        <p>Coconut water lowers blood sugar and heightens joy.</p>
+        
         <p>Enter your information below for a chance to win free coconuts for life!</p>
       </section>
       
